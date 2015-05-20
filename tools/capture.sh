4442a9f2535c31eb869c83c2bc1a2025b4bf86f6
Tarjei Husøy <git@thusoy.com>
2015-05-20 15:14:06 +0200

    Clean up cluster tools a bit

--- tools/capture.sh
+++ tools/capture.sh
@@ -4,18 +4,17 @@
 
 grep $(hostname) apply-case.py
 
-ntpdate -q no.pool.ntp.org | tail -1 | grep -o "offset.*"
 python clock.py &
 clock_pid=$!
 trap cleanup INT
 
 function cleanup () {
-	kill -9 $clock_pid
-	echo -e "\n"
-	exit $?
+    kill -9 $clock_pid
+    echo -e "\n"
+    exit $?
 }
 
 while :; do
-	xwd -root | gzip > /tmp/screen-$(hostname)-$(date +"%s").xwd.gz
-	sleep 10
+    gm import -window root /tmp/screen-$(hostname)-$(date +"%s").png
+    sleep 10
 done
--- tools/clock.py
+++ tools/clock.py
@@ -3,29 +3,29 @@ import sys
 import subprocess
 
 def print_clock_offset():
-	ntpdate_output = subprocess.check_output([
-		'ntpdate',
-		'-q',
-		'no.pool.ntp.org',
-		]).strip().split('\n')
-	offset_start = ntpdate_output[-1].find('offset')
-	return ntpdate_output[-1][offset_start:]
+    ntpdate_output = subprocess.check_output([
+        'ntpdate',
+        '-q',
+        'no.pool.ntp.org',
+        ]).strip().split('\n')
+    offset_start = ntpdate_output[-1].find('offset')
+    return ntpdate_output[-1][offset_start:]
 
 def now():
-        return time.time()
+    return time.time()
 
 def start_printing_time():
-	while True:
-	    try:
-		    t = now()
-		    print '%.3f' % t,
-		    sys.stdout.flush()
-		    time.sleep(0.005)
-		    print '\r',
-	    except KeyboardInterrupt:
-		break
+    while True:
+        try:
+            t = now()
+            print '%.3f' % t,
+            sys.stdout.flush()
+            time.sleep(0.005)
+            print '\r',
+        except KeyboardInterrupt:
+            break
 
 
 if __name__ == '__main__':
-#	print print_clock_offset()
-	start_printing_time()
+    print print_clock_offset()
+    start_printing_time()
