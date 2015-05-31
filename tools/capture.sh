d33783ebd2f1b2b7cce7b562bc45386aaa601e97
Tarjei Husøy <git@thusoy.com>
2015-05-31 14:11:11 +0200

    Use tcpdump for bw monitoring

--- tools/capture.sh
+++ tools/capture.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
 # Starts the actual capture of screenshots, and prints the current time offset and local clock
+session_id=$(hostname)-$(date +"%s")
 
+# Print which role we're configured as
 grep $(hostname) apply-case.py
+
+# Start sniffing traffic to reconstruct bandwidth usage between peers, excluding some common spammy services (ssh, spotify, name lookups, ssdp)
+sudo tcpdump -nNqtts68 "port not 3271 and port not 17500 and port not 137 and port not 138 and port not 1900 and (udp or tcp)" -w /tmp/$session-id.pcap &
+tcpdump_pid=$!
 
 python clock.py &
 clock_pid=$!
 trap cleanup INT
 
 function cleanup () {
-    kill -9 $clock_pid
-    echo -e "\n"
+    echo -e "\nStopping children..."
+    kill -INT $clock_pid
+    kill -INT $tcpdump_pid
     exit $?
 }
 
 while :; do
-    gm import -window root /tmp/screen-$(hostname)-$(date +"%s").png
+    gm import -window root /tmp/screen-$session_id-$(date +"%s").png
     sleep 10
 done
