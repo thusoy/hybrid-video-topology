dbbe30bee3184844303529fca43a87a96ca7eefa
Tarjei Husøy <thusoy@users.noreply.github.com>
2015-06-01 16:02:52 +0200

    Echo session_id on termination

--- tools/capture.sh
+++ tools/capture.sh
@@ -9,7 +9,7 @@ session_id=$(hostname)-$(date +"%s")
 iface=$(ifconfig | grep "inet addr:$(curl -s canhazip.com)" -B1 | head -1 | cut -d" " -f1)
 
 # Start sniffing traffic to reconstruct bandwidth usage between peers, excluding some common spammy services (ssh, spotify, name lookups, ssdp)
-sudo tcpdump -i $iface -nNqtts68 "port not 3271 and port not 17500 and port not 137 and port not 138 and port not 1900 and (udp or tcp)" -w /tmp/$session-id.pcap 2> /dev/null &
+sudo tcpdump -i $iface -nNqtts68 "port not 3271 and port not 17500 and port not 137 and port not 138 and port not 1900 and (udp or tcp)" -w /tmp/$session_id.pcap 2> /dev/null &
 tcpdump_pid=$!
 
 python clock.py &
@@ -17,7 +17,7 @@ clock_pid=$!
 trap cleanup INT
 
 function cleanup () {
-    echo -e "\nStopping children..."
+    echo -e "\nStopped session $session_id"
     kill -9 $clock_pid
     sudo kill -INT $tcpdump_pid
     exit $?
