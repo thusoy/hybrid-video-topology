554ade63121c50ffd4ac3c38fea31afc18d1bbb0
Tarjei Husøy <thusoy@users.noreply.github.com>
2015-06-01 16:17:14 +0200

    Echo ip when ending capture

--- tools/capture.sh
+++ tools/capture.sh
@@ -5,8 +5,8 @@ session_id=$(hostname)-$(date +"%s")
 
 # Print which role we're configured as
 # grep $(hostname) apply-case.py
-
-iface=$(ifconfig | grep "inet addr:$(curl -s canhazip.com)" -B1 | head -1 | cut -d" " -f1)
+my_ip=$(curl -s canhazip.com)
+iface=$(ifconfig | grep "inet addr:$my_ip" -B1 | head -1 | cut -d" " -f1)
 
 # Start sniffing traffic to reconstruct bandwidth usage between peers, excluding some common spammy services (ssh, spotify, name lookups, ssdp)
 sudo tcpdump -i $iface -nNqtts68 "port not 3271 and port not 17500 and port not 137 and port not 138 and port not 1900 and (udp or tcp)" -w /tmp/$session_id.pcap 2> /dev/null &
@@ -17,7 +17,7 @@ clock_pid=$!
 trap cleanup INT
 
 function cleanup () {
-    echo -e "\nStopped session $session_id"
+    echo -e "\$session_id/$my_ip"
     kill -9 $clock_pid
     sudo kill -INT $tcpdump_pid
     exit $?
