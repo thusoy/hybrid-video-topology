034c2ae38864080eb0af7e839ad4ec698c9f0589
Tarjei Husøy <git@thusoy.com>
2015-06-06 23:13:32 +0200

    Capture correct fields from pcaps

--- tools/extract_csv_data_from_pcap.sh
+++ tools/extract_csv_data_from_pcap.sh
@@ -8,7 +8,7 @@ filters=$(python -c "import sys; print ','.join(sys.argv[2:])" $@)
 tshark -r $1 -qz io,stat,1,$filters | sed "1,/0 <>/d" | tr -d "|" | python -c "
 import sys
 num_filters = len(sys.argv[2:])
-field_numbers = [0] + range(4,4+num_filters*2, 2)
+field_numbers = [0] + range(6,6+num_filters*2, 2)
 print 'Interval start,' + ','.join(sys.argv[2:])
 for line in sys.stdin:
     if line.startswith('===='):
