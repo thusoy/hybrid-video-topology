92cc3327185b68d10b4cb0bd10ae0121d2cf0b7d
Tarjei Husøy <git@thusoy.com>
2015-06-18 22:00:04 +0200

    Don't be selective on keys

--- tools/getstats.js
+++ tools/getstats.js
@@ -4,7 +4,7 @@ github.com/muaz-khan/getStats, MIT license)
 */
 
 (function () {
-    function getPrivateStats(peer, callback) {
+    function getPrivateStats(peer, callback, keysToSkip) {
         _getStats(peer, function (reports) {
             var result = {
                 audio: {},
@@ -22,15 +22,15 @@ github.com/muaz-khan/getStats, MIT license)
                     isBandwidthEstimation = report.type == 'VideoBwe';
 
                 if (isIncomingAudio) {
-                    result.audio.incoming = parseIncomingAudioReport(report);
+                    result.audio.incoming = parseReport(report, 'audio.incoming.', keysToSkip);
                 } else if (isOutgoingAudio) {
-                    result.audio.outgoing = parseOutgoingAudioReport(report);
+                    result.audio.outgoing = parseReport(report, 'audio.outgoing.', keysToSkip);
                 } else if (isIncomingVideo) {
-                    result.video.incoming = parseIncomingVideoReport(report);
+                    result.video.incoming = parseReport(report, 'video.incoming.', keysToSkip);
                 } else if (isOutgoingVideo) {
-                    result.video.outgoing = parseOutgoingVideoReport(report);
+                    result.video.outgoing = parseReport(report, 'video.outgoing.', keysToSkip);
                 } else if (isBandwidthEstimation) {
-                    result.video.bandwidth = parseBandwidthEstimationReport(report);
+                    result.video.bandwidth = parseReport(report, 'video.bandwidth.', keysToSkip);
                 } else if (isConnectionInUse) {
                     // report.googActiveConnection means either STUN or TURN is used.
                     result.connection = parseConnectionReport(report);
@@ -46,134 +46,16 @@ github.com/muaz-khan/getStats, MIT license)
         });
     }
 
-    function parseIncomingAudioReport(report) {
-        return {
-            audioOutputLevel: report.audioOutputLevel, // "23"
-            bytesReceived: report.bytesReceived, // "16983"
-            captureStartNtpTimeMs: report.googCaptureStartNtpTimeMs, // "-1"
-            codecName: report.googCodecName, // "opus"
-            currentDelayMs: report.googCurrentDelayMs, // "100"
-            decodingCNG: report.googDecodingCNG, // "0"
-            decodingCTN: report.googDecodingCTN, // "356"
-            decodingCTSG: report.googDecodingCTSG, // "0"
-            decodingNormal: report.googDecodingNormal, // "356"
-            decodingPLC: report.googDecodingPLC, // "0"
-            decodingPLCCNG: report.googDecodingPLCCNG, // "0"
-            expandRate: report.googExpandRate, // "0"
-            jitterBufferMs: report.googJitterBufferMs, // "43"
-            jitterReceived: report.googJitterReceived, // "0"
-            preferredJitterBufferMs: report.googPreferredJitterBufferMs, // "20"
-            trackId: report.googTrackId, // "431a02bf-8913-47a6-807c-882881ea6eb2"
-            id: report.id, // "ssrc_2174837908_recv"
-            packetsLost: report.packetsLost, // "0"
-            packetsReceived: report.packetsReceived, // "179"
-            // ssrc: report.ssrc, // "2174837908"
-            // timestamp: report.timestamp, // Fri Jun 12 2015 15:23:27 GMT+0200 (CEST)
-            // transportId: report.transportId, // "Channel-audio-1"
-            // type: report.type, // "ssrc"
-        };
-    }
-
-    function parseOutgoingAudioReport(report) {
-        return {
-            audioInputLevel: report.audioInputLevel, // "15"
-            bytesSent: report.bytesSent, // "16505"
-            codecName: report.googCodecName, // "opus"
-            echoCancellationEchoDelayMedian: report.googEchoCancellationEchoDelayMedian, // "-12"
-            echoCancellationEchoDelayStdDev: report.googEchoCancellationEchoDelayStdDev, // "0"
-            echoCancellationQualityMin: report.googEchoCancellationQualityMin, // "-1"
-            echoCancellationReturnLoss: report.googEchoCancellationReturnLoss, // "-100"
-            echoCancellationReturnLossEnhancement: report.googEchoCancellationReturnLossEnhancement, // "-100"
-            jitterReceived: report.googJitterReceived, // "-1"
-            rtt: report.googRtt, // "-1"
-            trackId: report.googTrackId, // "558be40c-82e7-4bb2-b20d-e2abcd3bc3ea"
-            typingNoiseState: report.googTypingNoiseState, // "false"
-            id: report.id, // "ssrc_3067768073_send"
-            packetsLost: report.packetsLost, // "-1"
-            packetsSent: report.packetsSent, // "180"
-            // ssrc: report.ssrc, // "3067768073"
-            // timestamp: report.timestamp, // Fri Jun 12 2015 15:23:27 GMT+0200 (CEST)
-            // transportId: report.transportId, // "Channel-audio-1"
-            // type: report.type, // "ssrc"
-        };
-    }
-
-    function parseIncomingVideoReport(report) {
-        return {
-            bytesReceived: report.bytesReceived, // "74557"
-            captureStartNtpTimeMs: report.googCaptureStartNtpTimeMs, // "3643104203674"
-            currentDelayMs: report.googCurrentDelayMs, // "25"
-            decodeMs: report.googDecodeMs, // "2"
-            firsSent: report.googFirsSent, // "0"
-            frameHeightReceived: report.googFrameHeightReceived, // "480"
-            frameRateDecoded: report.googFrameRateDecoded, // "0"
-            frameRateOutput: report.googFrameRateOutput, // "0"
-            frameRateReceived: report.googFrameRateReceived, // "30"
-            frameWidthReceived: report.googFrameWidthReceived, // "640"
-            jitterBufferMs: report.googJitterBufferMs, // "11"
-            maxDecodeMs: report.googMaxDecodeMs, // "4"
-            minPlayoutDelayMs: report.googMinPlayoutDelayMs, // "0"
-            nacksSent: report.googNacksSent, // "0"
-            plisSent: report.googPlisSent, // "0"
-            renderDelayMs: report.googRenderDelayMs, // "10"
-            targetDelayMs: report.googTargetDelayMs, // "25"
-            trackId: report.googTrackId, // "e2eaa689-2da2-43b3-96a0-3a3213d77716"
-            id: report.id, // "ssrc_3102970513_recv"
-            packetsLost: report.packetsLost, // "0"
-            packetsReceived: report.packetsReceived, // "109"
-            // ssrc: report.ssrc, // "3102970513"
-            // timestamp: report.timestamp, // Fri Jun 12 2015 15:23:27 GMT+0200 (CEST)
-            // transportId: report.transportId, // "Channel-audio-1"
-            // type: report.type, // "ssrc"
-        };
-    }
-
-    function parseOutgoingVideoReport(report) {
-        return {
-            bytesSent: report.bytesSent, // "106680"
-            adaptationChanges: report.googAdaptationChanges, // "0"
-            avgEncodeMs: report.googAvgEncodeMs, // "8"
-            bandwidthLimitedResolution: report.googBandwidthLimitedResolution, // "false"
-            captureJitterMs: report.googCaptureJitterMs, // "24"
-            captureQueueDelayMsPerS: report.googCaptureQueueDelayMsPerS, // "0"
-            codecName: report.googCodecName, // "VP8"
-            cpuLimitedResolution: report.googCpuLimitedResolution, // "false"
-            encodeUsagePercent: report.googEncodeUsagePercent, // "70"
-            firsReceived: report.googFirsReceived, // "0"
-            frameHeightInput: report.googFrameHeightInput, // "480"
-            frameHeightSent: report.googFrameHeightSent, // "480"
-            frameRateInput: report.googFrameRateInput, // "28"
-            frameRateSent: report.googFrameRateSent, // "30"
-            frameWidthInput: report.googFrameWidthInput, // "640"
-            frameWidthSent: report.googFrameWidthSent, // "640"
-            nacksReceived: report.googNacksReceived, // "0"
-            plisReceived: report.googPlisReceived, // "0"
-            rtt: report.googRtt, // "81"
-            trackId: report.googTrackId, // "3b51fae2-d996-4e35-b240-f93818f96ff4"
-            viewLimitedResolution: report.googViewLimitedResolution, // "false"
-            id: report.id, // "ssrc_3480135701_send"
-            packetsLost: report.packetsLost, // "0"
-            packetsSent: report.packetsSent, // "131"
-            // ssrc: report.ssrc, // "3480135701"
-            // timestamp: report.timestamp, // Fri Jun 12 2015 15:15:28 GMT+0200 (CEST)
-            // transportId: report.transportId, // "Channel-audio-1"
-            // type: report.type, // "ssrc"
-        };
-    }
-
-    function parseBandwidthEstimationReport(report) {
-        return {
-            googActualEncBitrate: report.googActualEncBitrate, // "247232"
-            googAvailableReceiveBandwidth: report.googAvailableReceiveBandwidth, // "188220"
-            googAvailableSendBandwidth: report.googAvailableSendBandwidth, // "381160"
-            googBucketDelay: report.googBucketDelay, // "0"
-            googRetransmitBitrate: report.googRetransmitBitrate, // "0"
-            googTargetEncBitrate: report.googTargetEncBitrate, // "282882"
-            googTransmitBitrate: report.googTransmitBitrate, // "333128"
-            // id: report.id, // "bweforvideo"
-            // timestamp: report.timestamp, // Fri Jun 12 2015 15:23:27 GMT+0200 (CEST)
-            // type: report.type, // "VideoBwe"
-        };
+    function parseReport(report, prefix, keysToSkip) {
+        var parsedReport = {};
+        for(var key in report) {
+            var qualifiedKeyName = prefix + key;
+            var shouldSkip = keysToSkip.indexOf(qualifiedKeyName) != -1;
+            if (report.hasOwnProperty(key) && !shouldSkip) {
+                parsedReport[key] = report[key];
+            }
+        }
+        return parsedReport;
     }
 
     function parseConnectionReport(report) {
@@ -228,8 +110,9 @@ github.com/muaz-khan/getStats, MIT license)
         }
     };
 
-    window.getStats = function (peer, callback) {
-        getPrivateStats(peer, callback);
+    window.getStats = function (peer, callback, keysToSkip) {
+        keysToSkip = keysToSkip || [];
+        getPrivateStats(peer, callback, keysToSkip);
     }
 })();
 
@@ -270,13 +153,21 @@ github.com/muaz-khan/getStats, MIT license)
         var ngInjector = angular.element(document.body).injector();
         var rtcmanager = ngInjector.get('RTCManager');
         var peerConnections = rtcmanager.getPeerConnections();
+        // TODO: Allow skipping entire categories by 'audio.*', or the same
+        // field in all categories, like '*.ssrc'
+        var skipList = [
+            'audio.incoming.ssrc',
+            'audio.outgoing.ssrc',
+            'video.incoming.ssrc',
+            'video.outgoing.ssrc',
+        ];
         for (var i = 0; i < peerConnections.length; i++) {
             var peerConnection = peerConnections[i];
-            getStats(peerConnection, reportAggregator);
+            getStats(peerConnection, reportAggregator, skipList);
         }
         setTimeout(printStats, 1000);
     }
-    var reportAggregator = aggregateReports();
+    var reportAggregator = createReportAggregator();
 
     console.log("Starting stats collection in 5s");
     setTimeout(printStats, 5000);
